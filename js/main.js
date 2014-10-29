ad11666763e27f04c6e6c5f5ec8f21dc942a9321
xiebaochun <463449055@qq.com>
2014-10-30 00:06:04 +0800

    update

--- js/main.js
+++ js/main.js
@@ -28,7 +28,30 @@ var timeCallBack;
   console.log(device_height);
  $("#index_img").css({"height":device_height+"px","width":device_width+"px"});
  $("#bottom_img").css({"height":device_height+"px","width":device_width+"px"});
- $("#next_img").css({"height":device_height+"px","width":device_width+"px"});
+
+ $("#tip_01").css({"height":device_height+"px","width":device_width+"px"});
+ 
+ // $("#tip_01").css({"left":device_width+"px","z-index":"1"});
+ $("#tip1_01").css({"height":"100%","width":"100%"});
+ $("#tip1_03").css({"max-width":"640px","width":"100%"});
+ $("#tip1_02").css({"max-width":"480px","width":"60%"});
+
+ $(".tips").css({"height":device_height+"px","width":device_width+"px"});
+
+  $(".bg").css({"height":"100%","width":"100%"});
+ $("#tip2_03").css({"max-width":"640px","width":"100%"});
+ $("#tip2_02").css({"max-width":"480px","width":"60%"});
+
+
+  $("#tip5_02").css({"max-width":"640px","width":"100%"});
+  $("#tip5_02").css({"left":(device_width-$("#tip5_02").width())/2+"px","top":"16%"});
+  $("#tip5_03").css({"max-width":"640px","width":"100%"});
+  $("#tip5_03").css({"left":(device_width-$("#tip5_03").width())/2+"px","top":"35%"});
+  $("#tip5_04").css({"max-width":"271px","width":"50%"});
+  $("#tip5_04").css({"left":"70%","top":"15%"});
+  $("#tip5_05").css({"max-width":"372px","width":"50%"});
+  $("#tip5_05").css({"left":(device_width-$("#tip5_05").width())/2+"px","bottom":"5%"});
+  
 
 $("#bottom_img").swipe({
       swipe:function(event, direction, distance, duration, fingerCount) {
@@ -48,170 +71,323 @@ $("#bottom_img").swipe({
           //     timeCallBack=setInterval(playGame,2000);
           //   }
            $("#bottom_img").css({"z-index":"1"});
-          if(direction=="left"){
-            $("#next_img").css({"left":device_width+"px","z-index":"2"});
+           
+           $("#tip_01").css({"z-index":"2"});
+
+           if(direction=="left"){
+            $("#tip_01").addClass("animated bounceInRight");
+            // $("#bottom_img").addClass("animated bounceOutLeft");
           }else{
-             $("#next_img").css({"top":device_height+"px","z-index":"2"});
+             $("#tip_01").addClass("animated bounceInUp");
+             
+             // $("#bottom_img").addClass("animated bounceOutRight");
           }
-            $("#next_img").attr("src","images/"+index+".jpg");
-            $("#next_img").show();
-            $("#next_img").animate({
-          
-              left: "0px",
-              top:"0px"
-              }, 1000, function() {
-              // Animation complete.
+          $('#tip_01').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+             isSwiping=false;
+             $("#tip1_03").show();
+             $("#tip1_03").addClass("animated fadeInDown");
+             $("#tip1_02").show();
+             $("#tip1_02").addClass("animated bounceInLeft");
+             $("#tip1_05").show();
+             $("#tip1_05").addClass("animated fadeInUp");
 
-              $("#bottom_img").css({"z-index":"1"});
-                        //$(this).attr("src","images/tu"+index+"_left-sheet0.png");
-              // $("#img_left_next").css("z-index","1");
-              //       $("#img_right_next").css("z-index","1");
-            
-                         isSwiping=false;
-                         //end();
-          });
+             $("#tip_01").removeClass("animated bounceInUp");
+             $("#tip_01").removeClass("animated bounceInRight");
 
-          
-
-          //     $("#img_right").animate({
-            
-          //   left: "+=50%",
-          //   top: "60%"
-          // }, 1000, function() {
-          //   $(this).css({"left":"15%","top":"20%","z-index":"0"});
-          //   $(this).attr("src","images/tu"+index+"_right-sheet0.png");
-          // });
-        }
-        if((direction=="right"||direction=="down")&&index>=3){
-            if(isSwiping)return;
-            isSwiping=true;
-            
-            console.log("You swiped " + direction );
-            index-=1;
-
-          //       count++;
-          //       $("#count_number").html(count);
-          //   if(!isStart){
-          //     isStart=true;
-          //     timeCallBack=setInterval(playGame,2000);
-          //   }
-           $("#bottom_img").css({"z-index":"1"});
-          $("#next_img").attr("src","images/"+index+".jpg");
-          if(direction=="right"){
-            $("#next_img").css({"left":-device_width+"px","z-index":"2"});
-          }else{
-             $("#next_img").css({"top":-device_height+"px","z-index":"2"});
-          }
-            
-            $("#next_img").show();
-            $("#next_img").animate({
-          
-              left: "0px",
-              top:"0px"
-              }, 1000, function() {
-              // Animation complete.
-              $("#bottom_img").css({"z-index":"1"});
-                        //$(this).attr("src","images/tu"+index+"_left-sheet0.png");
-              // $("#img_left_next").css("z-index","1");
-              //       $("#img_right_next").css("z-index","1");
-            
-                         isSwiping=false;
-                         //end();
+            $("#bottom_img").css({"z-index":"0"});
+           
+           
+             // $("#bottom_img").removeClass("animated bounceOutLeft");
+             // $("#bottom_img").removeClass("animated bounceOutRight");
+             
           });
         }
       }
     });
+// function addEventTip_01(){
 
-$("#next_img").swipe({
-      swipe:function(event, direction, distance, duration, fingerCount) {
-          
-          if((direction=="left"||direction=="up")&&index<=6){
-
-            if(isSwiping)return;
-            isSwiping=true;
+  $("#tip_01").swipe({
+        swipe:function(event, direction, distance, duration, fingerCount) {
             
-            console.log("You swiped " + direction );
-            index+=1;
+            if((direction=="left"||direction=="up")&&index<=6){
 
-          //       count++;
-          //       $("#count_number").html(count);
-          //   if(!isStart){
-          //     isStart=true;
-          //     timeCallBack=setInterval(playGame,2000);
-          //   }
-          $("#next_img").css({"z-index":"1"});
-          if(direction=="left"){
-            $("#bottom_img").css({"left":device_width+"px","z-index":"2"});
-          }else{
-             $("#bottom_img").css({"top":device_height+"px","z-index":"2"});
+              if(isSwiping)return;
+              isSwiping=true;
+              
+              console.log("You swiped " + direction );
+              index+=1;
+
+          
+            $("#tip_01").css({"z-index":"1"});
+            $("#tip_02").css({"z-index":"2"});
+             if(direction=="left"){
+              $("#tip_02").addClass("animated bounceInRight");
+            }else{
+               $("#tip_02").addClass("animated bounceInUp");
+            }
+
+             $('#tip_02').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                 isSwiping=false;
+                 $("#tip2_03").show();
+                 $("#tip2_03").addClass("animated fadeInDown");
+                 $("#tip2_02").show();
+                 $("#tip2_02").addClass("animated bounceInRight");
+                 $("#tip2_05").show();
+                 $("#tip2_05").addClass("animated fadeInUp");
+                2
+               $("#tip_02").removeClass("animated bounceInRight");
+               $("#tip_02").removeClass("animated bounceInRight");
+                
+               $("#tip_01").css({"z-index":"0"});
+               
+            
+            });
+            
           }
-            $("#bottom_img").attr("src","images/"+index+".jpg");
-            $("#bottom_img").show();
-            $("#bottom_img").animate({
-          
-              left: "0px",
-              top:"0px"
-              }, 1000, function() {
-              // Animation complete.
-              $("#next_img").css({"z-index":"1"});
-                        //$(this).attr("src","images/tu"+index+"_left-sheet0.png");
-              // $("#img_left_next").css("z-index","1");
-              //       $("#img_right_next").css("z-index","1");
-            
-                         isSwiping=false;
-                         //end();
+          if((direction=="right"||direction=="down")&&index>=3){
+              if(isSwiping)return;
+              isSwiping=true;
+              
+              console.log("You swiped " + direction );
+              index-=1;
+
+           
+            $("#tip_01").css({"z-index":"1"});
+            $("#bottom_img").css({"z-index":"2"});
+             if(direction=="right"){
+              $("#bottom_img").addClass("animated bounceInLeft");
+            }else{
+               $("#bottom_img").addClass("animated bounceInDown");
+            }
+            $('#bottom_img').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+               isSwiping=false;
+               
+
+             $("#bottom_img").removeClass("animated bounceInDown");
+             $("#bottom_img").removeClass("animated bounceInLeft");
+            $("#tip_01").css({"z-index":"0"});
           });
-
-          
-
-          //     $("#img_right").animate({
-            
-          //   left: "+=50%",
-          //   top: "60%"
-          // }, 1000, function() {
-          //   $(this).css({"left":"15%","top":"20%","z-index":"0"});
-          //   $(this).attr("src","images/tu"+index+"_right-sheet0.png");
-          // });
-        }
-        if((direction=="right"||direction=="down")&&index>=3){
-            if(isSwiping)return;
-            isSwiping=true;
-            
-            console.log("You swiped " + direction );
-            index-=1;
-
-          //       count++;
-          //       $("#count_number").html(count);
-          //   if(!isStart){
-          //     isStart=true;
-          //     timeCallBack=setInterval(playGame,2000);
-          //   }
-          $("#next_img").css({"z-index":"1"});
-          $("#bottom_img").attr("src","images/"+index+".jpg");
-          if(direction=="right"){
-            $("#bottom_img").css({"left":-device_width+"px","z-index":"2"});
-          }else{
-             $("#bottom_img").css({"top":-device_height+"px","z-index":"2"});
+           
           }
-            
-            $("#bottom_img").show();
-            $("#bottom_img").animate({
-          
-              left: "0px",
-              top:"0px"
-              }, 1000, function() {
-              // Animation complete.
-              $("#next_img").css({"z-index":"1"});
-                        //$(this).attr("src","images/tu"+index+"_left-sheet0.png");
-              // $("#img_left_next").css("z-index","1");
-              //       $("#img_right_next").css("z-index","1");
-            
-                         isSwiping=false;
-                         //end();
-          });
         }
-      }
-    });
+      });
+
+ $("#tip_02").swipe({
+        swipe:function(event, direction, distance, duration, fingerCount) {
+            
+            if((direction=="left"||direction=="up")&&index<=6){
+
+              if(isSwiping)return;
+              isSwiping=true;
+              
+              console.log("You swiped " + direction );
+              index+=1;
+
+          
+            $("#tip_02").css({"z-index":"1"});
+            $("#tip_03").css({"z-index":"2"});
+             if(direction=="left"){
+              $("#tip_03").addClass("animated bounceInRight");
+            }else{
+               $("#tip_03").addClass("animated bounceInUp");
+            }
+             $('#tip_03').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+               isSwiping=false;
+                 $("#tip3_03").show();
+                 $("#tip3_03").addClass("animated fadeInDown");
+                 $("#tip3_02").show();
+                 $("#tip3_02").addClass("animated bounceInRight");
+                 $("#tip3_05").show();
+                 $("#tip3_05").addClass("animated fadeInUp");
+             $("#tip_03").removeClass("animated bounceInRight");
+             $("#tip_03").removeClass("animated bounceInUp");
+             $("#tip_02").css({"z-index":"0"});
+            
+          });
+          }
+          if((direction=="right"||direction=="down")&&index>=3){
+              if(isSwiping)return;
+              isSwiping=true;
+              
+              console.log("You swiped " + direction );
+              index-=1;
+
+           
+            $("#tip_02").css({"z-index":"1"});
+            $("#tip_01").css({"z-index":"2"});
+             if(direction=="right"){
+              $("#tip_01").addClass("animated bounceInLeft");
+            }else{
+               $("#tip_01").addClass("animated bounceInDown");
+            }
+            $('#tip_01').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+               isSwiping=false;
+             $("#tip_01").removeClass("animated bounceInDown");
+             $("#tip_01").removeClass("animated bounceInLeft");
+             $("#tip_02").css({"z-index":"0"});
+            
+          });
+           
+          }
+        }
+      });
+
+ $("#tip_03").swipe({
+        swipe:function(event, direction, distance, duration, fingerCount) {
+            
+            if((direction=="left"||direction=="up")&&index<=6){
+
+              if(isSwiping)return;
+              isSwiping=true;
+              
+              console.log("You swiped " + direction );
+              index+=1;
+
+          
+            $("#tip_03").css({"z-index":"1"});
+            $("#tip_04").css({"z-index":"2"});
+             if(direction=="left"){
+              $("#tip_04").addClass("animated bounceInRight");
+            }else{
+               $("#tip_04").addClass("animated bounceInUp");
+            }
+             $('#tip_04').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+               isSwiping=false;
+                 $("#tip4_03").show();
+                 $("#tip4_03").addClass("animated fadeInDown");
+                 $("#tip4_02").show();
+                 $("#tip4_02").addClass("animated bounceInRight");
+                 $("#tip4_05").show();
+                 $("#tip4_05").addClass("animated fadeInUp");
+             $("#tip_04").removeClass("animated bounceInRight");
+             $("#tip_04").removeClass("animated bounceInUp");
+             $("#tip_03").css({"z-index":"0"});
+            
+          });
+          }
+          if((direction=="right"||direction=="down")&&index>=3){
+              if(isSwiping)return;
+              isSwiping=true;
+              
+              console.log("You swiped " + direction );
+              index-=1;
+
+           
+            $("#tip_03").css({"z-index":"1"});
+            $("#tip_02").css({"z-index":"2"});
+             if(direction=="right"){
+              $("#tip_02").addClass("animated bounceInLeft");
+            }else{
+               $("#tip_02").addClass("animated bounceInDown");
+            }
+            $('#tip_02').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+               isSwiping=false;
+             $("#tip_02").removeClass("animated bounceInDown");
+             $("#tip_02").removeClass("animated bounceInLeft");
+             $("#tip_03").css({"z-index":"0"});
+            
+          });
+           
+          }
+        }
+      });
+
+ $("#tip_04").swipe({
+        swipe:function(event, direction, distance, duration, fingerCount) {
+            
+            if((direction=="left"||direction=="up")&&index<=6){
+
+              if(isSwiping)return;
+              isSwiping=true;
+              
+              console.log("You swiped " + direction );
+              index+=1;
+
+          
+              $("#tip_04").css({"z-index":"1"});
+              $("#tip_05").css({"z-index":"2"});
+              if(direction=="left"){
+                $("#tip_05").addClass("animated bounceInRight");
+              }else{
+                 $("#tip_05").addClass("animated bounceInUp");
+              }
+               $('#tip_05').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                   isSwiping=false;
+                   $("#tip5_03").show();
+                   $("#tip5_03").addClass("animated fadeInDown");
+                   $("#tip5_02").show();
+                   $("#tip5_02").addClass("animated bounceInRight");
+
+                   $("#tip5_04").show();
+                   $("#tip5_04").addClass("animated bounceInRight");
+                   $("#tip5_05").show();
+
+                   $("#tip5_05").addClass("animated fadeInUp");
+
+                 $("#tip_05").removeClass("animated bounceInRight");
+                 $("#tip_05").removeClass("animated bounceInUp");
+                 $("#tip_04").css({"z-index":"0"});
+            
+              });
+          }
+          if((direction=="right"||direction=="down")&&index>=3){
+              if(isSwiping)return;
+              isSwiping=true;
+              
+              console.log("You swiped " + direction );
+              index-=1;
+
+           
+            $("#tip_04").css({"z-index":"1"});
+            $("#tip_03").css({"z-index":"2"});
+             if(direction=="right"){
+              $("#tip_03").addClass("animated bounceInLeft");
+            }else{
+               $("#tip_03").addClass("animated bounceInDown");
+            }
+            $('#tip_03').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+               isSwiping=false;
+             $("#tip_03").removeClass("animated bounceInDown");
+             $("#tip_03").removeClass("animated bounceInLeft");
+             $("#tip_04").css({"z-index":"0"});
+            
+          });
+           
+          }
+        }
+      });
+
+$("#tip_05").swipe({
+        swipe:function(event, direction, distance, duration, fingerCount) {
+            
+            
+          if((direction=="right"||direction=="down")&&index>=3){
+              if(isSwiping)return;
+              isSwiping=true;
+              
+              console.log("You swiped " + direction );
+              index-=1;
+
+           
+            $("#tip_05").css({"z-index":"1"});
+            $("#tip_04").css({"z-index":"2"});
+             if(direction=="right"){
+              $("#tip_04").addClass("animated bounceInLeft");
+            }else{
+               $("#tip_04").addClass("animated bounceInDown");
+            }
+            $('#tip_04').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+               isSwiping=false;
+             $("#tip_04").removeClass("animated bounceInDown");
+             $("#tip_04").removeClass("animated bounceInLeft");
+             $("#tip_05").css({"z-index":"0"});
+            
+          });
+           
+          }
+        }
+      });
+// }
   function preload() {
           for (i = 0; i < preload.arguments.length; i++) {
             images[i] = new Image()
